8c8bbc313993a27dbb7543176b8cb5f795af137a
Anand21J-V <anandvishwakarma21j.com>
2025-05-16 15:30:30 +0530

    run app.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Professional Summary Generator',
+    version= '0.0.0',
+    author= 'Anand Kumar Vishwakarma',
+    author_email= 'anandvishwakarma21j@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
